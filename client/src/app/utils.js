49ea21478f8cd4c631a8d6138fbaf483639247e7
Rick Zamudio <ricardo.ii.zamudio@gmail.com>
2020-04-02 20:45:55 -0500

    Add screenshots for README

--- client/src/app/utils.js
+++ client/src/app/utils.js
@@ -1,6 +1,6 @@
 export const capos = Array.from(Array(10).keys())
 export const decades = [2020, 2010, 2000, 1990, 1980, 1970, 1960]
-export const genres =['pop', 'punk', 'emo', 'folk', 'country', 'electronic', 'soundtrack']
+export const genres =['pop', 'rock', 'punk', 'emo', 'folk', 'country', 'electronic', 'soundtrack']
 
 export const capitalizeFirstLetter = (word) => {
   return word.charAt(0).toUpperCase() + word.slice(1);
